5c0eadc75c20b948862487567b60586ada5a1673
d0gkiller87 <32069685+d0gkiller87@users.noreply.github.com>
2025-01-09 05:58:00 +0800

    Add a button to support switching models without reloading the page

--- userscript.js
+++ userscript.js
@@ -1,41 +1,135 @@
 // ==UserScript==
-// @name         ChatGPT use 4o-mini model instead of 4o
+// @name         ChatGPT model switcher: switch on/off 4o-mini at will!
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Intercepts completion requests and inject {model: gpt-4o-mini} into the request
+// @version      0.2
+// @description  Injects a button allowing you to switch on/off 4o-mini during the chat
 // @match        *://chatgpt.com/*
-// @grant        none
 // @author       d0gkiller87
 // @license      MIT
+// @grant        unsafeWindow
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @run-at       document-end
 // ==/UserScript==
 
-(function() {
+(async function() {
   'use strict';
 
-  // Store the original fetch function
-  const originalFetch = window.fetch;
-
-  // Override the fetch function
-  window.fetch = async (resource, config = {}) => {
-    // Check if the config has a JSON body in a POST request
-    if (
-      resource == 'https://chatgpt.com/backend-api/conversation' &&
-      config.method === 'POST' &&
-      config.headers &&
-      config.headers['Content-Type'] === 'application/json' &&
-      config.body
-    ) {
-      // Parse the JSON body
-      const body = JSON.parse(config.body);
-
-      // Add "debug": true to the JSON payload
-      body.model = 'gpt-4o-mini';
-
-      // Update the config body with the modified JSON
-      config.body = JSON.stringify(body);
+  class ModelSwitcher {
+    constructor( useMini = true ) {
+      this.useMini = useMini;
     }
 
-    // Call the original fetch with modified config
-    return originalFetch(resource, config);
-  };
+    hookFetch() {
+      const originalFetch = unsafeWindow.fetch;
+      unsafeWindow.fetch = async ( resource, config = {} ) => {
+        if (
+          this.useMini &&
+          resource === 'https://chatgpt.com/backend-api/conversation' &&
+          config.method === 'POST' &&
+          config.headers &&
+          config.headers['Content-Type'] === 'application/json' &&
+          config.body
+        ) {
+          const body = JSON.parse( config.body );
+          body.model = 'gpt-4o-mini';
+          config.body = JSON.stringify( body );
+        }
+        return originalFetch( resource, config );
+      };
+    }
+
+    injectToggleButton() {
+      // Credit: https://webdevworkshop.io/code/css-toggle-button/
+      if ( !document.getElementById( 'toggleCss' ) ) {
+        const styleNode = document.createElement( 'style' );
+        styleNode.id = 'toggleCss';
+        styleNode.type = 'text/css';
+        styleNode.textContent = `.toggle {
+  position: relative;
+  display: inline-block;
+  width: 2.5rem;
+  height: 1.5rem;
+  background-color: hsl(0deg 0% 40%);
+  border-radius: 25px;
+  cursor: pointer;
+  transition: background-color 0.25s ease-in;
+}
+.toggle::after {
+  content: '';
+  position: absolute;
+  width: 1.4rem;
+  left: 0.1rem;
+  height: calc(1.5rem - 2px);
+  top: 1px;
+  background-color: white;
+  border-radius: 50%;
+  transition: all 0.25s ease-out;
+}
+#cb-toggle:checked + .toggle {
+  background-color: hsl(102, 58%, 39%);
+}
+#cb-toggle:checked + .toggle::after {
+  transform: translateX(1rem);
+}
+.hide-me {
+  opacity: 0;
+  height: 0;
+  width: 0;
+}`;
+        document.head.appendChild( styleNode );
+      }
+
+      const container = document.querySelector( '#composer-background div:nth-of-type(2) div:first-child' );
+      if ( container ) {
+        container.classList.add( 'items-center' );
+
+        const observer = new MutationObserver( mutationsList => {
+          for ( const mutation of mutationsList ) {
+            if ( mutation.type !== 'childList' ) continue;
+            if ( !mutation.addedNodes ) continue;
+            observer.disconnect();
+
+            setTimeout(() => {
+              // <input id="cb-toggle" type="checkbox" class="hide-me">
+              const checkbox = document.createElement( 'input' );
+              checkbox.id = 'cb-toggle';
+              checkbox.type = 'checkbox';
+              checkbox.className = 'hide-me';
+              checkbox.checked = this.useMini;
+
+              // <label for="cb-toggle" class="toggle" title="Toggle GPT-4o Mini"></label>
+              const label = document.createElement( 'label' );
+              label.htmlFor = 'cb-toggle';
+              label.className = 'toggle';
+              label.title = 'Using model: GPT-4o' + ( this.useMini ? ' mini' : '' );
+
+              container.appendChild( checkbox );
+              container.appendChild( label );
+
+              const cb = document.querySelector( '#cb-toggle' );
+              cb.addEventListener(
+                'click', async () => {
+                  this.useMini = cb.checked;
+                  await GM.setValue( 'useMini', this.useMini );
+                  label.title = 'Using model: GPT-4o' + ( this.useMini ? ' mini' : '' );
+                },
+                false
+              );
+            }, 1000 );
+
+            break;
+          }
+        });
+        observer.observe( container, { childList: true } );
+      } else {
+        console.error( 'container not found!' );
+      }
+    }
+  }
+
+  const useMini = await GM.getValue( 'useMini', true );
+  const switcher = new ModelSwitcher( useMini );
+  switcher.hookFetch();
+  switcher.injectToggleButton();
 })();
