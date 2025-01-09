3dd958be454d7af3e4ebc704939d065a12c23d7e
d0gkiller87 <32069685+d0gkiller87@users.noreply.github.com>
2025-01-09 11:50:00 +0800

    Fix: button disappearing after switching conversations

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT Model Switcher: Toggle on/off 4o-mini
 // @namespace    http://tampermonkey.net/
-// @version      0.21
+// @version      0.22
 // @description  Injects a button allowing you to toggle on/off 4o-mini during the chat
 // @match        *://chatgpt.com/*
 // @author       d0gkiller87
@@ -9,7 +9,7 @@
 // @grant        unsafeWindow
 // @grant        GM.setValue
 // @grant        GM.getValue
-// @run-at       document-end
+// @run-at       document-idle
 // ==/UserScript==
 
 (async function() {
@@ -39,7 +39,7 @@
       };
     }
 
-    injectToggleButton() {
+    injectToggleButtonStyle() {
       // Credit: https://webdevworkshop.io/code/css-toggle-button/
       if ( !document.getElementById( 'toggleCss' ) ) {
         const styleNode = document.createElement( 'style' );
@@ -53,7 +53,7 @@
   background-color: hsl(0deg 0% 40%);
   border-radius: 25px;
   cursor: pointer;
-  transition: background-color 0.25s ease-in;
+  transition: background-color 0.2s ease-in;
 }
 .toggle::after {
   content: '';
@@ -64,7 +64,7 @@
   top: 1px;
   background-color: white;
   border-radius: 50%;
-  transition: all 0.25s ease-out;
+  transition: all 0.2s ease-out;
 }
 #cb-toggle:checked + .toggle {
   background-color: hsl(102, 58%, 39%);
@@ -79,44 +79,49 @@
 }`;
         document.head.appendChild( styleNode );
       }
+    }
 
+    injectToggleButton( container = null ) {
+      if ( !container ) container = document.querySelector( '#composer-background div:nth-of-type(2) div:first-child' )
+      container.classList.add( 'items-center' );
+
+      // <input id="cb-toggle" type="checkbox" class="hide-me">
+      const checkbox = document.createElement( 'input' );
+      checkbox.id = 'cb-toggle';
+      checkbox.type = 'checkbox';
+      checkbox.className = 'hide-me';
+      checkbox.checked = this.useMini;
+
+      // <label for="cb-toggle" class="toggle" title="Toggle GPT-4o Mini"></label>
+      const label = document.createElement( 'label' );
+      label.htmlFor = 'cb-toggle';
+      label.className = 'toggle';
+      label.title = `Using model: ${ this.useMini ? 'GPT-4o mini' : 'original' }`;
+
+      container.appendChild( checkbox );
+      container.appendChild( label );
+
+      const cb = document.querySelector( '#cb-toggle' );
+      cb.addEventListener(
+        'click', async () => {
+          this.useMini = cb.checked;
+          await GM.setValue( 'useMini', this.useMini );
+          label.title = `Using model: ${ this.useMini ? 'GPT-4o mini' : 'original' }`;
+        },
+        false
+      );
+    }
+
+    monitorButtons() {
       const container = document.querySelector( '#composer-background div:nth-of-type(2) div:first-child' );
       if ( container ) {
-        container.classList.add( 'items-center' );
-
         const observer = new MutationObserver( mutationsList => {
           for ( const mutation of mutationsList ) {
             if ( mutation.type !== 'childList' ) continue;
             if ( !mutation.addedNodes ) continue;
             observer.disconnect();
 
-            setTimeout(() => {
-              // <input id="cb-toggle" type="checkbox" class="hide-me">
-              const checkbox = document.createElement( 'input' );
-              checkbox.id = 'cb-toggle';
-              checkbox.type = 'checkbox';
-              checkbox.className = 'hide-me';
-              checkbox.checked = this.useMini;
-
-              // <label for="cb-toggle" class="toggle" title="Toggle GPT-4o Mini"></label>
-              const label = document.createElement( 'label' );
-              label.htmlFor = 'cb-toggle';
-              label.className = 'toggle';
-              label.title = `Using model: ${ this.useMini ? 'GPT-4o mini' : 'original' }`;
-
-              container.appendChild( checkbox );
-              container.appendChild( label );
-
-              const cb = document.querySelector( '#cb-toggle' );
-              cb.addEventListener(
-                'click', async () => {
-                  this.useMini = cb.checked;
-                  await GM.setValue( 'useMini', this.useMini );
-                  label.title = `Using model: ${ this.useMini ? 'GPT-4o mini' : 'original' }`;
-                },
-                false
-              );
-            }, 1000 );
+            setTimeout( () => this.injectToggleButton( container ), 1000 );
 
             break;
           }
@@ -126,10 +131,22 @@
         console.error( 'container not found!' );
       }
     }
+
+    monitorMain() {
+      const mainNode = document.querySelector( 'body main' );
+
+      const observer = new MutationObserver( mutationsList => {
+        this.injectToggleButton();
+      });
+
+      observer.observe( mainNode, { childList: true } );
+    }
   }
 
   const useMini = await GM.getValue( 'useMini', true );
   const switcher = new ModelSwitcher( useMini );
   switcher.hookFetch();
-  switcher.injectToggleButton();
+  switcher.injectToggleButtonStyle();
+  switcher.monitorButtons();
+  switcher.monitorMain();
 })();
